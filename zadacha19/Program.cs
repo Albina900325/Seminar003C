2411cf32bf4b8087cba8793be39bc2d82b0d9e1f
Albina <albisha900325@gmail.com>
2023-01-14 11:40:37 +0500

    zadacha19

--- zadacha19/Program.cs
+++ zadacha19/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Ведите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+   if ( a % 100%10 == a % 10000%1000%100%10 && a / 10000 == a % 10)
+
+        Console.WriteLine("Да");
+
+  else Console.WriteLine("Нет");
+//Не очень коретное решение
