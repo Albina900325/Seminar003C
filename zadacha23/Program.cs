2a6d121e3405c6ecda2d2a66910e6fd8455b4d50
Albina <albisha900325@gmail.com>
2023-01-14 11:08:44 +0500

    zadacha23

--- zadacha23/Program.cs
+++ zadacha23/Program.cs
@@ -1,5 +1,15 @@
 ﻿Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
+//int i = 1;
+
+//while (i < a)
+//{ Console.Write(i*i*i + " ");
+//i++;
+//}
+
+for (int i=1; i<= a; i++)
+      Console.Write($"{i*i*i} ");
+ 
 
 
